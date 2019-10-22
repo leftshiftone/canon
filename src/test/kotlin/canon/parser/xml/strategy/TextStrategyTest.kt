aa62fd52a78562af105a43dbf675980f4da422d0
mallera <andreas.maller@leftshift.one>
2019-10-22 14:24:04 +0200

    [GAIA-1036] (encoding and decoding + tests)

---
+++ src/test/kotlin/canon/parser/xml/strategy/TextStrategyTest.kt
@@ -0,0 +1,27 @@
+package canon.parser.xml.strategy
+
+import canon.extension.toNode
+import canon.parser.xml.CanonXmlParser
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+class TextStrategyTest {
+
+    @Test
+    fun testParse() {
+        val xml = "<text id='testId' class='testClass' regex='regEx' placeholder='*' " +
+                "required='true' name='testName'>test</text>"
+        val parsed = TextStrategy().parse(xml.toNode(), HashMap<String, Any?>(), CanonXmlParser()::toRenderables)
+
+        assertNotNull(parsed)
+        assertEquals("testId", parsed.id)
+        assertEquals("testClass", parsed.`class`)
+        assertEquals("regEx", parsed.regex)
+        assertEquals("*", parsed.placeholder)
+        assertEquals(true, parsed.required)
+        assertEquals("testName", parsed.name)
+        assertEquals("test", parsed.value)
+    }
+
+}

a15567a12e6431e6d38e9207bcda899f60024f10
Michael Mair <michael.mair@leftshift.one>
2019-11-21 16:53:09 +0100

    removed json map to node parser

--- src/main/kotlin/canon/parser/xml/CanonXmlParser.kt
+++ src/main/kotlin/canon/parser/xml/CanonXmlParser.kt
@@ -24,6 +24,7 @@ open class CanonXmlParser {
             DOCUMENT_BUILDER.set(DOCUMENT_BUILDER_FACTORY.get().newDocumentBuilder())
         }
 
+        @JvmStatic
         fun getDocumentBuilder(): DocumentBuilder {
             // TODO: refactor!
             if (DOCUMENT_BUILDER_FACTORY.get() == null)

55863adc958985cbc36e2f079bac5391255c5914
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2020-01-13 16:47:17 +0100

    centerLat & centerLng will be evaluated by Atreus

--- src/test/kotlin/canon/model/MapTest.kt
+++ src/test/kotlin/canon/model/MapTest.kt
@@ -10,7 +10,7 @@ class MapTest {
     @Test
     fun testMapMapping() {
         val mapped = Map(id = "testId", `class` = "testClass", name = "testName",
-                src = "testSrc", mapType = "type", centerLng = 123.0, centerLat = -170.0,
+                src = "testSrc", mapType = "type", centerLng = "123.0", centerLat = "-170.0",
                 markerIcon = "static/icons/icon01.svg", selectedMarkerIcon = "static/icons/icon02.svg",
                 routePoints = "45.3,46.8;45.8,44.7", centerBrowserLocation = false, required = true,
                 zoom = 12, maxSelections = 1).toMap(HashMap(), TestEvaluator())
@@ -19,8 +19,8 @@ class MapTest {
         assertThat(mapped["name"]).isEqualTo("testName")
         assertThat(mapped["src"]).isEqualTo("testSrc")
         assertThat(mapped["mapType"]).isEqualTo("type")
-        assertThat(mapped["centerLng"]).isEqualTo(123.0)
-        assertThat(mapped["centerLat"]).isEqualTo(-170.0)
+        assertThat(mapped["centerLng"]).isEqualTo("123.0")
+        assertThat(mapped["centerLat"]).isEqualTo("-170.0")
         assertThat(mapped["markerIcon"]).isEqualTo("static/icons/icon01.svg")
         assertThat(mapped["selectedMarkerIcon"]).isEqualTo("static/icons/icon02.svg")
         assertThat(mapped["routePoints"]).isEqualTo("45.3,46.8;45.8,44.7")

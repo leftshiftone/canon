b190e135b02836044fe717f29d4fcb54a7a9e3d0
Alberto Gea <alberto.geaperez@leftshift.one>
2020-03-13 15:02:12 +0100

    [GAIA-1337] Remove duplicated code in test

--- src/test/kotlin/canon/parser/xml/CanonXmlParserTest.kt
+++ src/test/kotlin/canon/parser/xml/CanonXmlParserTest.kt
@@ -7,19 +7,6 @@ import java.util.concurrent.TimeUnit
 
 class CanonXmlParserTest {
 
-    private fun parseXml(path: String, expectSuccess: Boolean = true) {
-        try {
-            val result = CanonXmlParser().parse(CanonXmlParserTest::class.java.getResourceAsStream(path))
-            Assertions.assertNotNull(result)
-            Assertions.assertTrue(result.isNotEmpty())
-        } catch (e: Exception) {
-            if (expectSuccess) {
-                Assertions.fail<String>("xml '$path' should not throw a validation exception", e)
-            }
-        }
-    }
-
-
     private fun parseXml(cannonParser: CanonXmlParser, path: String, expectSuccess: Boolean = true) {
         try {
             val result = cannonParser.parse(CanonXmlParserTest::class.java.getResourceAsStream(path))
@@ -33,127 +20,127 @@ class CanonXmlParserTest {
     }
 
     @Test
-    fun testComplex1() = parseXml("/xml/complex1.xml")
+    fun testComplex1() = parseXml(CanonXmlParser(),"/xml/complex1.xml")
 
     @Test
-    fun testComplex2() = parseXml("/xml/complex2.xml")
+    fun testComplex2() = parseXml(CanonXmlParser(),"/xml/complex2.xml")
 
     @Test
-    fun span() = parseXml("/xml/span.xml", false)
+    fun span() = parseXml(CanonXmlParser(),"/xml/span.xml", false)
 
     @Test
-    fun items() = parseXml("/xml/items.xml")
+    fun items() = parseXml(CanonXmlParser(),"/xml/items.xml")
 
     @Test
-    fun text1() = parseXml("/xml/text1.xml")
+    fun text1() = parseXml(CanonXmlParser(),"/xml/text1.xml")
 
     @Test
-    fun text2() = parseXml("/xml/text2.xml")
+    fun text2() = parseXml(CanonXmlParser(),"/xml/text2.xml")
 
     @Test
-    fun table1() = parseXml("/xml/table1.xml")
+    fun table1() = parseXml(CanonXmlParser(),"/xml/table1.xml")
 
     @Test
-    fun table2() = parseXml("/xml/table2.xml", false)
+    fun table2() = parseXml(CanonXmlParser(),"/xml/table2.xml", false)
 
     @Test
-    fun carousel1() = parseXml("/xml/carousel1.xml", false)
+    fun carousel1() = parseXml(CanonXmlParser(),"/xml/carousel1.xml", false)
 
     @Test
-    fun carousel2() = parseXml("/xml/carousel2.xml", false)
+    fun carousel2() = parseXml(CanonXmlParser(),"/xml/carousel2.xml", false)
 
     @Test
-    fun carousel3() = parseXml("/xml/carousel3.xml", false)
+    fun carousel3() = parseXml(CanonXmlParser(),"/xml/carousel3.xml", false)
 
     @Test
-    fun carousel4() = parseXml("/xml/carousel4.xml")
+    fun carousel4() = parseXml(CanonXmlParser(),"/xml/carousel4.xml")
 
     @Test
-    fun carousel5() = parseXml("/xml/carousel5.xml")
+    fun carousel5() = parseXml(CanonXmlParser(),"/xml/carousel5.xml")
 
     @Test
-    fun block() = parseXml("/xml/block.xml", false)
+    fun block() = parseXml(CanonXmlParser(),"/xml/block.xml", false)
 
     @Test
-    fun form1() = parseXml("/xml/form1.xml", false)
+    fun form1() = parseXml(CanonXmlParser(),"/xml/form1.xml", false)
 
     @Test
-    fun form2() = parseXml("/xml/form2.xml", false)
+    fun form2() = parseXml(CanonXmlParser(),"/xml/form2.xml", false)
 
     @Test
-    fun form3() = parseXml("/xml/form3.xml")
+    fun form3() = parseXml(CanonXmlParser(),"/xml/form3.xml")
 
     @Test
-    fun form4() = parseXml("/xml/form4.xml")
+    fun form4() = parseXml(CanonXmlParser(),"/xml/form4.xml")
 
     @Test
-    fun form5() = parseXml("/xml/form5.xml", false)
+    fun form5() = parseXml(CanonXmlParser(),"/xml/form5.xml", false)
 
     @Test
-    fun datepicker1() = parseXml("/xml/datepicker1.xml", false)
+    fun datepicker1() = parseXml(CanonXmlParser(),"/xml/datepicker1.xml", false)
 
     @Test
-    fun datepicker2() = parseXml("/xml/datepicker2.xml", false)
+    fun datepicker2() = parseXml(CanonXmlParser(),"/xml/datepicker2.xml", false)
 
     @Test
-    fun datepicker3() = parseXml("/xml/datepicker3.xml", false)
+    fun datepicker3() = parseXml(CanonXmlParser(),"/xml/datepicker3.xml", false)
 
     @Test
-    fun datepicker4() = parseXml("/xml/datepicker4.xml", false)
+    fun datepicker4() = parseXml(CanonXmlParser(),"/xml/datepicker4.xml", false)
 
     @Test
-    fun transition1() = parseXml("/xml/transition1.xml", false)
+    fun transition1() = parseXml(CanonXmlParser(),"/xml/transition1.xml", false)
 
     @Test
-    fun transition2() = parseXml("/xml/transition2.xml", false)
+    fun transition2() = parseXml(CanonXmlParser(),"/xml/transition2.xml", false)
 
     @Test
-    fun overlay1() = parseXml("/xml/overlay1.xml", false)
+    fun overlay1() = parseXml(CanonXmlParser(),"/xml/overlay1.xml", false)
 
     @Test
-    fun basket1() = parseXml("/xml/basket1.xml")
+    fun basket1() = parseXml(CanonXmlParser(),"/xml/basket1.xml")
 
     @Test
-    fun multipleChoice1() = parseXml("/xml/multipleChoice1.xml", false)
+    fun multipleChoice1() = parseXml(CanonXmlParser(),"/xml/multipleChoice1.xml", false)
 
     @Test
-    fun multipleChoice2() = parseXml("/xml/multipleChoice2.xml")
+    fun multipleChoice2() = parseXml(CanonXmlParser(),"/xml/multipleChoice2.xml")
 
     @Test
-    fun singleChoice1() = parseXml("/xml/singleChoice1.xml", false)
+    fun singleChoice1() = parseXml(CanonXmlParser(),"/xml/singleChoice1.xml", false)
 
     @Test
-    fun singleChoice2() = parseXml("/xml/singleChoice2.xml")
+    fun singleChoice2() = parseXml(CanonXmlParser(),"/xml/singleChoice2.xml")
 
     @Test
-    fun singleChoice3() = parseXml("/xml/singleChoice3.xml")
+    fun singleChoice3() = parseXml(CanonXmlParser(),"/xml/singleChoice3.xml")
 
     @Test
-    fun singleChoice4() = parseXml("/xml/singleChoice4.xml", false)
+    fun singleChoice4() = parseXml(CanonXmlParser(),"/xml/singleChoice4.xml", false)
 
     @Test
-    fun choiceWithSieve1() = parseXml("/xml/choiceWithSieve1.xml")
+    fun choiceWithSieve1() = parseXml(CanonXmlParser(),"/xml/choiceWithSieve1.xml")
 
     @Test
-    fun choiceWithSieve2() = parseXml("/xml/choiceWithSieve2.xml", false)
+    fun choiceWithSieve2() = parseXml(CanonXmlParser(),"/xml/choiceWithSieve2.xml", false)
 
     @Test
-    fun choiceWithSieve3() = parseXml("/xml/choiceWithSieve3.xml", false)
+    fun choiceWithSieve3() = parseXml(CanonXmlParser(),"/xml/choiceWithSieve3.xml", false)
 
     @Test
-    fun choiceWithSieve4() = parseXml("/xml/choiceWithSieve4.xml", false)
+    fun choiceWithSieve4() = parseXml(CanonXmlParser(),"/xml/choiceWithSieve4.xml", false)
 
     @Test
-    fun choiceWithSieve5() = parseXml("/xml/choiceWithSieve5.xml")
+    fun choiceWithSieve5() = parseXml(CanonXmlParser(),"/xml/choiceWithSieve5.xml")
 
     @Test
-    fun nameArg1() = parseXml("/xml/nameArg1.xml")
+    fun nameArg1() = parseXml(CanonXmlParser(),"/xml/nameArg1.xml")
 
     @Test
-    fun nameArg2() = parseXml("/xml/nameArg2.xml", false)
+    fun nameArg2() = parseXml(CanonXmlParser(),"/xml/nameArg2.xml", false)
 
     @Test
-    fun nameArg3() = parseXml("/xml/nameArg3.xml", false)
+    fun nameArg3() = parseXml(CanonXmlParser(),"/xml/nameArg3.xml", false)
 
 
     @Test

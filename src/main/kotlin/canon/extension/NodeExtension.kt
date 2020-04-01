6a8fcb56b775b8e9b5b331f8aa64abfbb505a3a7
malleran <andreas.maller@leftshift.one>
2020-04-01 15:48:58 +0200

    [SONAR] (sonar fixes)

--- src/main/kotlin/canon/extension/NodeExtension.kt
+++ src/main/kotlin/canon/extension/NodeExtension.kt
@@ -14,19 +14,21 @@ internal fun Node.attrAsBoolean(name: String, defaultValue: Boolean): Boolean {
     val optional = Optional.ofNullable(this.attributes.getNamedItem(name))
     return optional.map { it.textContent.equals("true", true) }.orElse(defaultValue)
 }
-internal fun Node.attrAsDouble(name:String, defaultValue:Double) : Double {
+
+internal fun Node.attrAsDouble(name: String, defaultValue: Double): Double {
     return try {
         if (this.attributes == null) return defaultValue
         this.attributes.getNamedItem(name).textContent.toDouble()
-    } catch (ex : RuntimeException) {
+    } catch (ex: RuntimeException) {
         defaultValue
     }
 }
-internal fun Node.attrAsInt(name:String, defaultValue:Int) : Int {
+
+internal fun Node.attrAsInt(name: String, defaultValue: Int): Int {
     return try {
         if (this.attributes == null) return defaultValue
         this.attributes.getNamedItem(name).textContent.toInt()
-    } catch (ex : RuntimeException) {
+    } catch (ex: RuntimeException) {
         defaultValue
     }
 }

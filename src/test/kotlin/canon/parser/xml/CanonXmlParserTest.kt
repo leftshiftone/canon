36ce832605e10b303a26d76feaf90afb60d894f6
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2020-04-24 10:49:59 +0200

    [GAIA-1427](xsd) modified singleChoiceType

--- src/test/kotlin/canon/parser/xml/CanonXmlParserTest.kt
+++ src/test/kotlin/canon/parser/xml/CanonXmlParserTest.kt
@@ -126,6 +126,9 @@ class CanonXmlParserTest {
     fun singleChoice4() = parseXml(CanonXmlParser(),"/xml/singleChoice4.xml", false)
 
     @Test
+    fun singleChoice6() = parseXml(CanonXmlParser(),"/xml/singleChoice6.xml", true)
+
+    @Test
     fun choiceWithSieve1() = parseXml(CanonXmlParser(),"/xml/choiceWithSieve1.xml")
 
     @Test

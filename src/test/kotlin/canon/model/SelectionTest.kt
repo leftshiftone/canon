25363d304e6e0047cc865d72e7f35e5973ead334
David Reindl <david.reindl@leftshift.one>
2020-05-07 09:14:25 +0200

    feature/GAIA-1441

--- src/test/kotlin/canon/model/SelectionTest.kt
+++ src/test/kotlin/canon/model/SelectionTest.kt
@@ -16,4 +16,17 @@ class SelectionTest {
         assertEquals("testName", mapped.get("name"))
         assertEquals(12, mapped.get("countdownInSec"))
     }
+
+    /*
+    @Test
+    fun testSelectionWithChoiceMapping() {
+        val item = Item("testId", "testClass", ArrayList()).toMap(HashMap(), TestEvaluator())
+
+        val selection = Selection("testId", "testClass", "testName", 12,
+                listOf(item)).toMap(HashMap(), TestEvaluator())
+
+        assertEquals(4, selection.size)
+        assertEquals("testName", selection.get("name"))
+        assertEquals(12, selection.get("countdownInSec"))
+    }*/
 }
